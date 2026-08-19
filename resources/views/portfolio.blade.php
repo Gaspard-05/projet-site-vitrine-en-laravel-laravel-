6b22bf7a0332f2e077783ca9f9d3805b3fecdf0f
Gaspard-05 <gaspardadiro@gmail.com>
2026-08-19 12:41:16 +0100

    feat: ajout des pages principales du site (about, contact, services, tarifs, portfolio, équipe)

---
+++ resources/views/portfolio.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Page Portfolio</title>
+</head>
+<body>
+    <h1>NOTRE PORTFOLIO</h1>
+    <p>Contenu de la page NOTRE PORTFOLIO</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/services', function () {
+    return view('services');
+});
+
+Route::get('/tarifs', function () {
+    return view('tarifs');
+});
+
+Route::get('/portfolio', function () {
+    return view('portfolio');
+});
+
+Route::get('/equipe', function () {
+    return view('equipe');
+});
